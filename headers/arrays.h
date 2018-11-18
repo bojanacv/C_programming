b38ca84a5ddf8e78c2d306caafded0a0644df720
bojanacv <bojana.cvitkovac@gmail.com>
2018-11-18 17:51:01 +0100

    First commit

---
+++ headers/arrays.h
@@ -0,0 +1,7 @@
+#ifndef __arrays_h__   //proverava da li vec postoji takav header fajl
+#define __arrays_h__
+
+void readArrayElements(int arr[], int arrLength);
+void printArrayElements(int arr[], int arrLength);
+
+#endif
---
+++ headers/sortalgs.h
@@ -0,0 +1,7 @@
+#ifndef __sortalgs_h__  
+#define __sortalgs_h__
+
+void bubbleSort(int arr[], int arrLength);
+
+
+#endif
